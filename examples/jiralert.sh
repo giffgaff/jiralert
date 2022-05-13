955f48f1190aaaf76379e404271f62748639d2b8
mandeepmsk <mandeep@giffgaff.co.uk>
2022-05-13 12:22:12 +0100

    [SRE-1238] Edit shell script to replace vars

--- examples/jiralert.sh
+++ examples/jiralert.sh
@@ -3,3 +3,5 @@ sed -i "s|api_url:|api_url: ${JIRALERT_URL}|g" "/config/jiralert.yml"
 sed -i "s|user:|user: ${JIRALERT_USER}|g" "/config/jiralert.yml"
 sed -i "s|password:|password: ${JIRALERT_PASS}|g" "/config/jiralert.yml"
 sed -i "s|project:|project: ${JIRALERT_PROJECT}|g" "/config/jiralert.yml"
+
+echo "this script has ran" > /tmp/jirascript.log
