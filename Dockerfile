cafef574f0689bf6a5792b563494567990c36112
12ushan <rushan@giffgaff.co.uk>
2022-05-17 16:07:34 +0100

    [SRE-1239] change dockerfile to run startup script

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,8 @@ RUN GO111MODULE=on GOBIN=/tmp/bin make
 FROM quay.io/prometheus/busybox-linux-amd64:latest
 
 COPY --from=builder /go/src/github.com/prometheus-community/jiralert/jiralert /bin/jiralert
-COPY /examples/jiralert.yml /config/jiralert.yml
 COPY /examples/jiralert.tmpl /config/jiralert.tmpl
 COPY /examples/jiralert.sh /config/jiralert.sh
 
-#RUN ./config/jiralert.sh
+CMD ["./config/jiralert.sh"]
 
-ENTRYPOINT ["./config/jiralert.sh"]
-CMD ["/bin/jiralert", "/tmp/jiralert.yml", "/tmp/jiralert.tmpl"]
--- examples/jiralert.sh
+++ examples/jiralert.sh
@@ -1,9 +1,41 @@
 #!/bin/sh
 
+cat << EOF > ../config/jiralert.yml
+---
+# Global defaults, applied to all receivers where not explicitly overridden. Optional.
+# Global defaults, applied to all receivers where not explicitly overridden. Optional.
+defaults:
+  # API access fields.
+  api_url: $JIRALERT_URL
+  user: $JIRALERT_USER
+  password: $JIRALERT_PASS
+  # The type of JIRA issue to create. Required.
+  issue_type: Story
+  # Issue priority. Optional.
+  priority: ""
+  # Go template invocation for generating the summary. Required.
+  summary: '{{ template "jira.summary" . }}'
+  # Go template invocation for generating the description. Optional.
+  description: '{{ template "jira.description" . }}'
+  # State to transition into when reopening a closed issue. Required.
+  reopen_state: "SELECTED FOR DEVELOPMENT"
+  # Do not reopen issues with this resolution. Optional.
+  wont_fix_resolution: "Won't Fix"
+  # Amount of time after being closed that an issue should be reopened, after which, a new issue is created.
+  # Optional (default: always reopen)
+  reopen_duration: 0h
 
-sed -i "s|api_url:|api_url: ${JIRALERT_URL}|g" "/config/jiralert.yml"
-sed -i "s|user:|user: ${JIRALERT_USER}|g" "/config/jiralert.yml"
-sed -i "s|password:|password: ${JIRALERT_PASS}|g" "/config/jiralert.yml"
-sed -i "s|project:|project: ${JIRALERT_PROJECT}|g" "/config/jiralert.yml"
+# Receiver definitions. At least one must be defined.
+receivers:
+  # Must match the Alertmanager receiver name. Required.
+  - name: 'jira-ab'
+    # JIRA project to create the issue in. Required.
+    project: $JIRA_PROJECT
+    # Copy all Prometheus labels into separate JIRA labels. Optional (default: false).
+    add_group_labels: true
 
-echo "this script has ran" > /tmp/jirascript.log
+# File containing template definitions. Required.
+template: jiralert.tmpl
+EOF
+
+/bin/jiralert /config/jiralert.yml /config/jiralert.tmpl
